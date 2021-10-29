29cd13717a095c1f4bbf3a7ff6e87396c9eade6e
Santiago León <85116254+Sant1001@users.noreply.github.com>
2021-10-29 16:07:37 -0500

    Update mainsnake.py

--- mainsnake.py
+++ mainsnake.py
@@ -47,9 +47,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9,color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, color2)
     update()
     ontimer(move, 100)
 
