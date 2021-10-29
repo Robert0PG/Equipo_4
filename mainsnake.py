de7252ca0462f9035091cfc04c3e628c2f347505
christianflober <89165453+christianflober@users.noreply.github.com>
2021-10-29 16:19:38 -0500

    Update mainsnake.py

--- mainsnake.py
+++ mainsnake.py
@@ -1,8 +1,7 @@
-"Programa que cuenta con una serpiente encargada de obtener su comida, si la serpiente se toca a si misma o choca en la pared se termina el juego"
-"Autores: Roberto Peláez García A01732317, Santiago León Salinas A01734958, Christian Flores Alberto A01734997"
-"Fecha: 29/10/2021"
+"""Programa que cuenta con una serpiente encargada de obtener su comida, si la serpiente se toca a si misma o choca en la pared se termina el juego
+Autores: Roberto Peláez García A01732317, Santiago León Salinas A01734958, Christian Flores Alberto A01734997
+Fecha: 29/10/2021"""
 from turtle import *
-
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -11,27 +10,27 @@ aim = vector(0, -10)
 
 
 def change(x, y):
-    "Funcion que cambia la direccion de la serpiente"
+    #Funcion que cambia la direccion de la serpiente
     aim.x = x
     aim.y = y
 
 
 def inside(head):
-    "Funcion que regresa si la cabeza de la serpiente se encuentra dentro del recuadro"
+    #Funcion que regresa si la cabeza de la serpiente se encuentra dentro del recuadro
     return -200 < head.x < 190 and -200 < head.y < 190
 
 colorbody=['blue','moccasin','black','green','purple']
-"Lista de colores que pueden ser elegidos para el cuerpo de la serpiente"
+#Lista de colores que pueden ser elegidos para el cuerpo de la serpiente
 color=random.choice(colorbody)
-"Funcion que regresa un color random"
+#Funcion que regresa un color random
 colorfood=['orange','cyan','gray','pink','gold']
-"Lista de colores que pueden ser elegidos para la comida"
+#Lista de colores que pueden ser elegidos para la comida
 color2=random.choice(colorfood)
-"Funcion que regresa un color random"
+#Funcion que regresa un color random
 
 
 def move():
-    "Funcion que mueve a la serpiente"
+    #Funcion que mueve a la serpiente
     head = snake[-1].copy()
     head.move(aim)
 
@@ -58,7 +57,7 @@ def move():
     update()
     ontimer(move, 100)
 
-"Setup inicial"
+#Setup inicial
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
