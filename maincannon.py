458c14c5208f9484450d43589ff59cc31f4db83b
Robert0PG <92008186+Robert0PG@users.noreply.github.com>
2021-10-29 16:12:41 -0500

    Update maincannon.py

--- maincannon.py
+++ maincannon.py
@@ -13,8 +13,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 15
-        speed.y = (y + 200) / 15
+        speed.x = (x + 200) / 14
+        speed.y = (y + 200) / 14
 
 
 def inside(xy):
@@ -45,7 +45,7 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 2
 
     if inside(ball):
         speed.y -= 0.35
